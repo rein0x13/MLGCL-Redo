300b104b62c5dd1371d3971d810dcb8fb8df2103
imantnttelzrs <antonettelozares6@gmail.com>
2024-04-29 15:45:00 +0800

    first commit

---
+++ database/migrations/2024_04_29_043008_create_subjects.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject_code');
+            $table->double('category');
+            $table->string('discriptive_title');
+            $table->string('prerequisites');
+            $table->string('subject_type');
+            $table->double('units');
+            $table->double('year_level');
+            $table->string('semester');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
