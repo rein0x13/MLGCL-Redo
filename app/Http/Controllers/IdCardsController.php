c3405e8890ec465e4e4a8c57540c99daf997e3cf
nelson john montezo 1 <nj.montezo12@gmail.com>
2024-04-29 15:52:47 +0800

    id cards table, controller, & model

---
+++ app/Http/Controllers/IdCardsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\idCards;
+use Illuminate\Http\Request;
+
+class IdCardsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(idCards $idCards)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(idCards $idCards)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, idCards $idCards)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(idCards $idCards)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_04_29_070527_create_id_cards_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('id_cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('profile_id');
+            $table->string('pictures');
+            $table->string('signature');
+            $table->string('validity');
+            $table->string('validation_start');
+            $table->string('validation_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('id_cards');
+    }
+};
