cd137e5a3d4f163de036702aa495d0be17fc6071
Rico <ricoinguito5@.com>
2024-04-29 15:20:22 +0800

    update

---
+++ database/migrations/2024_04_29_071753_create_grades_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->string('profile_id');
+            $table->string('subject_id');
+            $table->string('grade');
+            $table->string('re_exam');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grades');
+    }
+};
