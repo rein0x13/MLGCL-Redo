dc6d4b0fcaa152f86fdd7d396ad258c2333fe6a6
rosemarie <rfaller989@gmail.com>
2024-04-29 15:06:42 +0800

    update

--- database/migrations/2024_04_29_042714_create_courses_table.php
+++ database/migrations/2024_04_29_042714_create_courses_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('course', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('code');
             $table->string('description');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('course');
+        Schema::dropIfExists('courses');
     }
 };
