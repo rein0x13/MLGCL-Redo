e08c31e66532cb213b097ffe523d452ee7b67d40
rein0x13 <nij13x@gmail.com>
2024-05-06 15:14:09 +0800

    Bugfix on attributes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,16 @@
-<x-head>
-    <div class="h-screen bg-gray d-flex justify-content-center align-items-center">
-        <div class="card p-4" style="width: 400px">
-            <div class="d-flex justify-content-center align-items-center">
-                <img src="{{asset('static/mlgcl_logo.png')}}" alt="">
-            </div>
-                <div class="mb-3">
-                  <label for="email" class="form-label">Email</label>
-                  <input type="email" class="form-control" id="email">
-                </div>
-                <div class="mb-3">
-                  <label for="password" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="password">
-                </div>
-                <button class="btn btn-primary mt-2">Login</button>
+<x-head class="h-screen bg-gray d-flex justify-content-center align-items-center">
+    <div class="card p-4" style="width: 400px">
+        <div class="d-flex justify-content-center align-items-center">
+            <img src="{{ asset('static/mlgcl_logo.png') }}" alt="">
         </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password">
+        </div>
+        <button class="btn btn-primary mt-2">Login</button>
     </div>
 </x-head>
