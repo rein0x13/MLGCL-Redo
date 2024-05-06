3cae5e0d6b688b198d533321c0b2fbc05d0f9105
rein0x13 <nij13x@gmail.com>
2024-05-06 15:04:51 +0800

    Initial Login Page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,89 +1,55 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>MLGCL Portal</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-    <style>
-        #sidebar {
-            width: 300px;
-            min-height: 100vh;
-        }
-
-        .font-sans {
-            font-family: Figtree, ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji
-        }
-
-        .bg-gray {
-            background-color: #f3f4f6;
-        }
-    </style>
-</head>
-
-<body class="d-flex font-sans">
-    <div class="bg-primary h" id="sidebar">
-        <div class="px-4">
-            {{-- <div class="container d-flex justify-content-center my-3"> --}}
-            <div class="my-3">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div><img src="{{ asset('static/mlgcl_logo.png') }}" height="100"
-                            alt="">
-                        </div>
-                        <div class="text-white fs-4">MLGCL Portal</div>
-                </div>
-            </div>
-            <div class="d-flex flex-column">
-                <button class="bg-transparent border-0 text-white py-2 text-start">Dashboard</button>
-                <button class="bg-transparent border-0 text-white py-2 text-start">Admission</button>
-                <button class="bg-transparent border-0 text-white py-2 text-start">QrCode Scanner</button>
-                <button class="bg-transparent border-0 text-white py-2 text-start">Links</button>
-                <button class="bg-transparent border-0 text-white py-2 text-start">Links</button>
-                <button class="bg-transparent border-0 text-white py-2 text-start">Links</button>
-                <div class="accordion accordion-flush" id="enrollment-accordion">
-                    <div class="accordion-item">
-                      <h2 class="accordion-header" id="enrollment-accordion-links">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#enrollment-links" aria-expanded="false" aria-controls="enrollment-links">
-                          Enrollments
-                        </button>
-                      </h2>
-                      <div id="enrollment-links" class="accordion-collapse collapse" aria-labelledby="enrollment-accordion-links" data-bs-parent="#enrollment-accordion">
-                        <div class="accordion-body d-flex flex-column">
-                            <button class="bg-transparent border-0 py-2 text-start">New Requests</button>
-                            <button class="bg-transparent border-0 py-2 text-start">For Enrollments</button>
-                            <button class="bg-transparent border-0 py-2 text-start">Rejected</button>
-                            <button class="bg-transparent border-0 py-2 text-start">Cashier's Hold</button>
-                        </div>
-                      </div>
+<x-head>
+    <body class="d-flex font-sans">
+        <div class="bg-primary h" id="sidebar">
+            <div class="px-4">
+                {{-- <div class="container d-flex justify-content-center my-3"> --}}
+                <div class="my-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div><img src="{{ asset('static/mlgcl_logo.png') }}" height="100"
+                                alt="">
+                            </div>
+                            <div class="text-white fs-4">MLGCL Portal</div>
                     </div>
                 </div>
-                <button class="bg-transparent border-0 text-white py-2 text-start">Links</button>
+                <div class="d-flex flex-column">
+                    <button class="bg-transparent border-0 text-white py-2 text-start">Dashboard</button>
+                    <button class="bg-transparent border-0 text-white py-2 text-start">Admission</button>
+                    <button class="bg-transparent border-0 text-white py-2 text-start">QrCode Scanner</button>
+                    <button class="bg-transparent border-0 text-white py-2 text-start">Links</button>
+                    <button class="bg-transparent border-0 text-white py-2 text-start">Links</button>
+                    <button class="bg-transparent border-0 text-white py-2 text-start">Links</button>
+                    <div class="accordion accordion-flush" id="enrollment-accordion">
+                        <div class="accordion-item">
+                          <h2 class="accordion-header" id="enrollment-accordion-links">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#enrollment-links" aria-expanded="false" aria-controls="enrollment-links">
+                              Enrollments
+                            </button>
+                          </h2>
+                          <div id="enrollment-links" class="accordion-collapse collapse" aria-labelledby="enrollment-accordion-links" data-bs-parent="#enrollment-accordion">
+                            <div class="accordion-body d-flex flex-column">
+                                <button class="bg-transparent border-0 py-2 text-start">New Requests</button>
+                                <button class="bg-transparent border-0 py-2 text-start">For Enrollments</button>
+                                <button class="bg-transparent border-0 py-2 text-start">Rejected</button>
+                                <button class="bg-transparent border-0 py-2 text-start">Cashier's Hold</button>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                    <button class="bg-transparent border-0 text-white py-2 text-start">Links</button>
 
+                </div>
             </div>
         </div>
-    </div>
-    <div class="bg-gray flex-fill">
-        <div class="bg-white py-3 px-4 fs-3">
-            {{ $heading ?? 'Dashboard' }}
-        </div>
-        <div>
-            <div class="card m-3 p-3">
-                {{$slot}}
+        <div class="bg-gray flex-fill">
+            <div class="bg-white py-3 px-4 fs-3">
+                {{ $heading ?? 'Dashboard' }}
+            </div>
+            <div>
+                <div class="card m-3 p-3">
+                    {{$slot}}
+                </div>
             </div>
         </div>
-    </div>
-</body>
-
-</html>
+    </body>
+</x-head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,71 +1,18 @@
-<!DOCTYPE html>
-<html lang="en" class="h-100">
-
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>MLGCL PORTAL</title>
-    <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('login/images/favicon.png') }}">
-    <!-- Custom Stylesheet -->
-    <link href="{{ asset('login/css/style.css') }}" rel="stylesheet">
-    
-</head>
-
-<body class="h-100">
-    <div id="preloader">
-        <div class="loader"></div>
-    </div>
-    <div class="login-bg h-100">
-        <div class="container h-100">
-            <div class="row justify-content-center h-100">
-                <div class="col-md-5">
-                    <div class="form-input-content">
-                        <div class="card card-login">
-                            <div class="card-header">
-                                <div class="nav-header position-relative  text-center w-100">
-                                    <div class="brand-logo">
-                                        <a href="index.html">
-                                            <b class="logo-abbr">D</b>
-                                            <span class="brand-title"><b>MLGCL PORTAL</b></span>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="text-center my-3">
-                                <img class="rounded-circle" src=" " width="80" height="80" alt="logo">
-                            </div>
-                            <div class="card-body">
-                                <form action="#">
-                                    <div class="form-group mb-4">
-                                        <input type="text" name="email" class="form-control rounded-0 bg-transparent" placeholder="Email">
-                                    </div>
-                                    <div class="form-group mb-4">
-                                        <input type="password" name="password" class="form-control rounded-0 bg-transparent" placeholder="Password">
-                                    </div>
-                                    <div class="form-group ml-3 mb-5">
-                                        <input id="checkbox1" type="checkbox">
-                                        <label class="label-checkbox ml-2 mb-0" for="checkbox1">Remember Password</label>
-                                    </div>
-                                    <button class="btn btn-primary btn-block border-0" type="submit">Login</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<x-head>
+    <div class="h-screen bg-gray d-flex justify-content-center align-items-center">
+        <div class="card p-4" style="width: 400px">
+            <div class="d-flex justify-content-center align-items-center">
+                <img src="{{asset('static/mlgcl_logo.png')}}" alt="">
             </div>
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="email">
+                </div>
+                <div class="mb-3">
+                  <label for="password" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="password">
+                </div>
+                <button class="btn btn-primary mt-2">Login</button>
         </div>
     </div>
-    <!-- #/ container -->
-    <!-- Common JS -->
-    <script src="{{ asset('login/plugins/common/common.min.js') }}"></script>
-    <!-- Custom script -->
-    <script src="{{ asset('login/js/custom.min.js') }}"></script>
-    <script src="{{ asset('login/js/settings.js') }}"></script>
-    <script src="{{ asset('login/js/quixnav.js') }}"></script>
-</body>
-
-
-</html>
+</x-head>
